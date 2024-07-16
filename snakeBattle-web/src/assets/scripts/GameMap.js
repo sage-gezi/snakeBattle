5d2cf77947ef2cf1a3059fa578ddacc2a742c1d6
sage <2785068762@qq.com>
2024-07-16 22:43:28 +0800

    画蛇 检测碰撞

--- snakeBattle-web/src/assets/scripts/GameMap.js
+++ snakeBattle-web/src/assets/scripts/GameMap.js
@@ -1,4 +1,5 @@
 import { GameObject } from './GameObject';
+import { Snake } from './Snake';
 import { Wall } from './Wall';
 
 export class GameMap extends GameObject {
@@ -9,15 +10,35 @@ export class GameMap extends GameObject {
     this.L = 0;
 
     this.rows = 13;
-    this.cols = 13;
+    this.cols = 14;
     this.inner_walls = 30;
     this.walls = [];
+
+    this.snakes = [
+      new Snake({ id: 0, color: '#4876EC', r: this.rows - 2, c: 1 }, this),
+      new Snake({ id: 1, color: '#F94848', r: 1, c: this.cols - 2 }, this)
+    ];
   }
   start() {
     for (let i = 0; i < 1000; i++) {
       if (this.create_wall()) {
-        return true;
+        break;
       }
+    }
+    this.add_listening_events();
+  }
+
+  check_ready() {
+    for (const snake of this.snakes) {
+      if (snake.status !== 'idle') return false;
+      if (snake.direction === -1) return false;
+    }
+    return true;
+  }
+
+  next_step() {
+    for (const snake of this.snakes) {
+      snake.next_step();
     }
   }
 
@@ -34,6 +55,24 @@ export class GameMap extends GameObject {
       }
     }
     return false;
+  }
+
+  add_listening_events() {
+    this.ctx.canvas.focus();
+    const [snake0, snake1] = this.snakes;
+    console.log(this.ctx.canvas);
+
+    this.ctx.canvas.addEventListener('keydown', (e) => {
+      console.log(e.key);
+      if (e.key === 'w') snake0.set_derection(0);
+      else if (e.key === 'd') snake0.set_derection(1);
+      else if (e.key === 's') snake0.set_derection(2);
+      else if (e.key === 'a') snake0.set_derection(3);
+      else if (e.key === 'ArrowUp') snake1.set_derection(0);
+      else if (e.key === 'ArrowRight') snake1.set_derection(1);
+      else if (e.key === 'ArrowDown') snake1.set_derection(2);
+      else if (e.key === 'ArrowLeft') snake1.set_derection(3);
+    });
   }
 
   create_wall() {
@@ -56,9 +95,9 @@ export class GameMap extends GameObject {
       for (let j = 0; j < 10000; j++) {
         let r = parseInt(Math.random() * this.rows);
         let c = parseInt(Math.random() * this.cols);
-        if (g[r][c] || g[c][r]) continue;
+        if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
         if ((r == this.rows - 2 && c == 1) || (r == 1 && c == this.cols - 2)) continue;
-        g[r][c] = g[c][r] = true;
+        g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
         break;
       }
     }
@@ -76,6 +115,27 @@ export class GameMap extends GameObject {
     }
     return true;
   }
+
+  check_valid(cell) {
+    for (const wall of this.walls) {
+      if (wall.r === cell.r && wall.c === cell.c) {
+        return false;
+      }
+    }
+    for (const snake of this.snakes) {
+      let k = snake.cells.length;
+      if (!snake.check_tail_increasing()) {
+        k--;
+      }
+      for (let i = 0; i < k; i++) {
+        if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
   update_size() {
     this.L = parseInt(
       Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows)
@@ -85,6 +145,9 @@ export class GameMap extends GameObject {
   }
   update() {
     this.update_size();
+    if (this.check_ready()) {
+      this.next_step();
+    }
     this.render();
   }
   render() {
